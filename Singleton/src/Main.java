08108981bd7324ec46a2af90be5eefed90af8ca8
dyfunttor <47650650+dyfunttor@users.noreply.github.com>
2020-09-25 23:16:56 -0300

    Update and rename Obj.java to Main.java

---
+++ Singleton/src/Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+
+    try {
+      //Get the only object available
+      MyFile object = MyFile.getInstance();
+
+      // read the file
+      String content = object.readFile();
+
+      // update content and write to file
+      System.out.println(content);
+      content += "(NOVO)";
+      object.writeFile(content);
+
+      // read the file again and print content
+      content = object.readFile();
+      System.out.println(content);
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
